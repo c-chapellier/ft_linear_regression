b865f14e8c8ef33e9c8cb817d1ae620447e8ca7f
Corentin s/o admin <corentin.chapellier@gmail.com>
2021-01-27 21:14:07 +0100

    every thing seems good

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ft_linear_regression
+
+Linear regression in python.
+
+pip3 install numpy pandas matplotlib
+
+### State {in progress}
---
+++ linear_regression.py
@@ -0,0 +1,112 @@
+import sys
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class linear_regression:
+
+  def __init__(self, dataset_path, learning_rate = 1, iterations = 100):
+    self.__learning_rate = learning_rate
+    self.__iterations = iterations
+    self.__df = pd.read_csv(dataset_path)
+    self.__x = self.__normalize(self.__df['km'])
+    self.__y = self.__normalize(self.__df['price'])
+    self.__errors = []
+    self.__theta0 = 0
+    self.__theta1 = 0
+    self.__thetas_path = "./thetas"
+    self.__is_trained = False
+
+  @staticmethod
+  def __normalize(numpyArray):
+    return (numpyArray - numpyArray.min(axis=0)) / (numpyArray.max(axis=0) - numpyArray.min(axis=0))
+
+  @staticmethod
+  def __denormalize(numpyArray, base):
+    return numpyArray * (base.max(axis=0) - base.min(axis=0)) + base.min(axis=0)
+
+  def __write_thetas(self):
+    y_0 = self.__denormalize(self.__estimate_price(self.__x[0]), self.__df['price'])
+    y_1 = self.__denormalize(self.__estimate_price(self.__x[1]), self.__df['price'])
+    x_0 = self.__denormalize(self.__x[0], self.__df['km'])
+    x_1 = self.__denormalize(self.__x[1], self.__df['km'])
+    theta1_denormalized = (y_1 - y_0) / (x_1 - x_0)
+    theta0_denormalized = y_0 - (theta1_denormalized * x_0)
+    with open(self.__thetas_path, "w") as file:
+      file.write(str(theta0_denormalized) + " ")
+      file.write(str(theta1_denormalized))
+
+  def __read_thetas(self):
+    with open(self.__thetas_path, "r") as file:
+      tmp = file.readlines()[0].split(" ")
+      self.__theta0 = float(tmp[0])
+      self.__theta1 = float(tmp[1])
+
+  def estimate_price_from_file(self, x):
+    try:
+      self.__read_thetas()
+    except IOError:
+      print("File not accessible")
+      return np.nan
+    return self.__estimate_price(x)
+
+  def __estimate_price(self, x):
+    return self.__theta0 + (self.__theta1 * x)
+
+  def train(self):
+    self.__theta0 = 0
+    self.__theta1 = 1
+    for itr in range(self.__iterations):
+      error_cost = 0
+      cost_theta0 = 0
+      cost_theta1 = 0
+      for i in range(len(self.__x)):
+        error = self.__estimate_price(self.__x[i]) - self.__y[i]
+        error_cost += error ** 2
+        cost_theta0 += error
+        cost_theta1 += self.__x[i] * error
+      self.__theta0 -= self.__learning_rate * (cost_theta0 / len(self.__x))
+      self.__theta1 -= self.__learning_rate * (cost_theta1 / len(self.__x))
+      self.__errors.append(error_cost)
+    self.__write_thetas()
+    self.__is_trained = True
+
+  def plot(self):
+    if self.__is_trained:
+      y_pred = self.__theta0 + (self.__theta1 * self.__x)
+      y_pred_denormalize = self.__denormalize(y_pred, self.__df['price'])
+      x_denormalize = self.__denormalize(self.__x, self.__df['km'])
+      y_denormalize = self.__denormalize(self.__y, self.__df['price'])
+      plt.scatter(x_denormalize, y_denormalize)
+      plt.plot(x_denormalize, y_pred_denormalize)
+      plt.ylabel("price")
+      plt.xlabel("mileage")
+      plt.show()
+      plt.figure(figsize=(10, 5))
+      plt.plot(np.arange(1, len(self.__errors) + 1), self.__errors, color='red', linewidth=5)
+      plt.ylabel("error")
+      plt.xlabel("iteration")
+      plt.show()
+    else:
+      print("Model not trained.")
+
+if __name__ == "__main__":
+  argc = len(sys.argv)
+  if argc < 2:
+    print("arg[1] must be train or predict")
+    exit()
+  model = linear_regression("./data.csv")
+  if sys.argv[1] == "train":
+    model.train()
+    model.plot()
+  elif sys.argv[1] == "predict":
+    if argc < 3:
+      print("arg[2] with predict must be the mileage")
+      exit()
+    try:
+      value = float(sys.argv[2])
+    except ValueError:
+      print("arg[2] with predict must be the mileage, a number")
+      exit()
+    print(model.estimate_price_from_file(value))
