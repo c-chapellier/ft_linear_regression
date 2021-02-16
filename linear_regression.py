0bfa6d0bd3abfa97e86fb8b1786c8a5de962f165
Corentin <corentin.chapellier@gmail.com>
2021-02-16 18:41:22 +0100

    3d_plot done

--- linear_regression.py
+++ linear_regression.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 class linear_regression:
 
@@ -12,7 +13,7 @@ class linear_regression:
     self.__df = pd.read_csv(dataset_path)
     self.__x = self.__normalize(self.__df['km'])
     self.__y = self.__normalize(self.__df['price'])
-    self.__errors = []
+    self.__errors = np.empty((3, self.__iterations))
     self.__theta0 = 0
     self.__theta1 = 0
     self.__thetas_path = "./thetas"
@@ -54,99 +55,116 @@ class linear_regression:
   def __estimate_price(self, x):
     return self.__theta0 + (self.__theta1 * x)
 
-  def train(self):
-    self.__theta0 = 0
-    self.__theta1 = 1
+  def train(self, starting_t0=0, starting_t1=1):
+    m = len(self.__x)
+    self.__theta0 = starting_t0
+    self.__theta1 = starting_t1
     for itr in range(self.__iterations):
+      self.__errors[0][itr] = self.__theta0
+      self.__errors[1][itr] = self.__theta1
       error_cost = 0
       cost_theta0 = 0
       cost_theta1 = 0
-      for i in range(len(self.__x)):
+      for i in range(m):
         error = self.__estimate_price(self.__x[i]) - self.__y[i]
-        error_cost += error ** 2
+        error_cost += (error ** 2) / m
         cost_theta0 += error
         cost_theta1 += self.__x[i] * error
-      self.__theta0 -= self.__learning_rate * (cost_theta0 / len(self.__x))
-      self.__theta1 -= self.__learning_rate * (cost_theta1 / len(self.__x))
-      self.__errors.append(error_cost)
+      self.__theta0 -= self.__learning_rate * (cost_theta0 / m)
+      self.__theta1 -= self.__learning_rate * (cost_theta1 / m)
+      self.__errors[2][itr] = error_cost
     self.__write_thetas()
     self.__is_trained = True
 
   def plot(self):
-    if self.__is_trained:
-      y_pred = self.__theta0 + (self.__theta1 * self.__x)
-      y_pred_denormalize = self.__denormalize(y_pred, self.__df['price'])
-      x_denormalize = self.__denormalize(self.__x, self.__df['km'])
-      y_denormalize = self.__denormalize(self.__y, self.__df['price'])
-      plt.scatter(x_denormalize, y_denormalize)
-      plt.plot(x_denormalize, y_pred_denormalize)
-      plt.ylabel("price")
-      plt.xlabel("mileage")
-      plt.show()
-      plt.figure(figsize=(10, 5))
-      plt.plot(np.arange(1, len(self.__errors) + 1), self.__errors, color='red', linewidth=5)
-      plt.ylabel("error")
-      plt.xlabel("iteration")
-      plt.show()
-    else:
+    if not self.__is_trained:
       print("Model not trained.")
+      exit()
+    y_pred = self.__theta0 + (self.__theta1 * self.__x)
+    y_pred_denormalize = self.__denormalize(y_pred, self.__df['price'])
+    x_denormalize = self.__denormalize(self.__x, self.__df['km'])
+    y_denormalize = self.__denormalize(self.__y, self.__df['price'])
+    plt.scatter(x_denormalize, y_denormalize)
+    plt.plot(x_denormalize, y_pred_denormalize)
+    plt.ylabel("price")
+    plt.xlabel("mileage")
+    plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.plot(np.arange(1, len(self.__errors[2]) + 1), self.__errors[2], color='red', linewidth=5)
+    plt.ylabel("error")
+    plt.xlabel("iterations")
+    plt.show()
 
   def plot_3d_cost(self):
+    if not self.__is_trained:
+      print("Model not trained.")
+      exit()
     grid_size = 40
+    thetas0 = np.empty((1, grid_size))
+    thetas1 = np.empty((1, grid_size))
     cost_grid = np.empty((grid_size, grid_size))
-
-    X = np.empty((1, grid_size))
-    Y = np.empty((1, grid_size))
-
     m = len(self.__x)
-    print(self.__theta0, self.__theta1)
 
-    for x_i in range(grid_size):
-      X[0][x_i] = self.__theta0 + ((x_i - (grid_size / 2)) * (self.__theta0 / 10))
-    for y_i in range(grid_size):
-      Y[0][y_i] = self.__theta1 + ((y_i - (grid_size / 2)) * (self.__theta1 / 5))
+    thetas0[0] = np.linspace(-5, 5, grid_size)
+    thetas1[0] = np.linspace(-5, 5, grid_size)
+    # for i in range(grid_size):
+    #   thetas0[0][i] = self.__theta0 + ((i - (grid_size / 2)) * (self.__theta0 / 0.5))
+    # for i in range(grid_size):
+    #   thetas1[0][i] = self.__theta1 + ((i - (grid_size / 2)) * (self.__theta1 / 0.5))
 
-    for x_i in range(grid_size):
-      for y_i in range(grid_size):
+    for i in range(grid_size):
+      for j in range(grid_size):
         cost = 0
-        for i in range(m):
-          error = (X[0][x_i] + (Y[0][y_i] * self.__x[i])) - self.__y[i]
+        for k in range(m):
+          error = (thetas0[0][i] + (thetas1[0][j] * self.__x[k])) - self.__y[k]
           cost += (error ** 2) / m
-        cost_grid[x_i][y_i] = cost
+        cost_grid[j][i] = cost
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+    thetas0, thetas1 = np.meshgrid(thetas0, thetas1)
 
-    X, Y = np.meshgrid(X, Y)
+    # create circle base
+    x0, y0, radius = self.__theta0, self.__theta1, 3.5
+    r = np.sqrt((thetas0 - x0)**2 + (thetas1 - y0)**2)
+    inside = r < radius
 
-    # Gx, Gy = np.gradient(cost_grid)
-    # G = ((Gx ** 2) + (Gy ** 2)) ** 0.5
-    # N = G / G.max()
-    # surf = ax.plot_surface(X, Y, cost_grid, rstride=1, cstride=1, facecolors=cm.jet(N), linewidth=0, antialiased=False, shade=False)
-
-    surf = ax.plot_surface(X, Y, cost_grid, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-
-    plt.title("cost of the model with thetas");
-    plt.xlabel("theta0")
-    plt.ylabel("theta1")
+    surf = ax.plot_trisurf(thetas0[inside], thetas1[inside], cost_grid[inside], alpha=.5, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax.scatter(self.__errors[0][:-1], self.__errors[1][:-1], self.__errors[2][:-1], c='blue', s=10)
+    ax.scatter(self.__errors[0][-1], self.__errors[1][-1], self.__errors[2][-1], c='red', s=100)
+    plt.title("cost of the model with thetas")
+    ax.set_xlabel("theta0")
+    ax.set_ylabel("theta1")
+    ax.set_zlabel("cost")
     plt.show()
+
 
 if __name__ == "__main__":
   argc = len(sys.argv)
   if argc < 2:
-    print("arg[1] must be train or predict")
+    print("arg[1] must be train or predict.")
     exit()
-  model = linear_regression("./data.csv")
+  model = linear_regression("./data.csv", learning_rate = 0.1, iterations = 1000)
   if sys.argv[1] == "train":
-    model.train()
+    if argc > 3:
+      try:
+        value, value2 = float(sys.argv[2]), float(sys.argv[3])
+      except ValueError:
+        print("arg[2] and arg[3] with train must be the starting thetas, numbers.")
+        print("-2 and -1 give a beautiful graph.")
+        exit()
+      model.train(value, value2)
+    else:
+      model.train()
     model.plot()
+    model.plot_3d_cost()
   elif sys.argv[1] == "predict":
     if argc < 3:
-      print("arg[2] with predict must be the mileage")
+      print("arg[2] with predict must be the mileage.")
       exit()
     try:
       value = float(sys.argv[2])
     except ValueError:
-      print("arg[2] with predict must be the mileage, a number")
+      print("arg[2] with predict must be the mileage, a number.")
       exit()
     print(model.estimate_price_from_file(value))
